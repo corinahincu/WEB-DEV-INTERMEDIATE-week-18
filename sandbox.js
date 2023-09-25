99c96bb098aaa068e24c60d857384362f32bfa49
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-25 23:24:03 +0200

    Add files via upload

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,30 @@
-var character = 'luigi';
-var age = 30;
-var isBlackBelt = false;
-//character = 20 - we can change the value, see the next line, but not the type, if I try to change the type of character, I have an error, but if I change the value, it's working .
-character = 'mario'; // ok
-//age = 'yoshi' errror: Type 'string' is not assignable to type 'number'
-age = 40; // ok
-// isBlackBelt = 'yes' -> error : Type 'string' is not assignable to type 'boolean'
-isBlackBelt = true; // ok
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays 
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+//names.push(3) -> error: Argument of type 'number' is not assignable to parameter of type 'string'
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun') error: Argument of type 'string' is not assignable to parameter of type 'number'.
+// numbers[1]= 'shaun' error: Type 'string' is not assignable to type 'number'.
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ruy');
+mixed.push(10);
+mixed[0] = 3;
+// in that case we have mixed arrays, as you can see, it works perfectly, with number, string etc, because it's already mixed types
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(7.5));
+ninja.age = 40; //ok
+ninja.name = 'ruy'; //ok
+// ninja.age = '30' -> error: Type 'string' is not assignable to type 'number'.
+// ninja.skills = ['fighting,sneaking'] error-> Property 'skills' does not exist on type '{ name: string; belt: string; age: number; }'.
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    /* age:40 */ // error: Property 'age' is missing in type '{ name: string; belt: string; }' but required in type '{ name: string; belt: string; age: number; }'. if we remove one of the property, we have an error, because we declared it above, and if we want to change them, we need to write every property, no exception.
+    age: 40
+    //skills:[] -> error: Object literal may only specify known properties, and 'skills' does not exist in type '{ name: string; belt: string; age: number; }'. the same thing, we can't add unknown properties.
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,17 +1,34 @@
-let character = 'luigi'
-let age = 30
-let isBlackBelt = false 
+// arrays 
+let names = [ 'luigi', 'mario','yoshi']
+names.push('toad')
+//names.push(3) -> error: Argument of type 'number' is not assignable to parameter of type 'string'
 
-//character = 20 - we can change the value, see the next line, but not the type, if I try to change the type of character, I have an error, but if I change the value, it's working .
-character = 'mario' // ok
-//age = 'yoshi' errror: Type 'string' is not assignable to type 'number'
-age = 40 // ok
+let numbers = [10,20,30,40]
+numbers.push(25)
+// numbers.push('shaun') error: Argument of type 'string' is not assignable to parameter of type 'number'.
+// numbers[1]= 'shaun' error: Type 'string' is not assignable to type 'number'.
 
-// isBlackBelt = 'yes' -> error : Type 'string' is not assignable to type 'boolean'
-isBlackBelt = true // ok
+let mixed = ['ken', 4, 'chun-li',8,9]
+mixed.push('ruy')
+mixed.push(10)
+mixed[0] = 3
+// in that case we have mixed arrays, as you can see, it works perfectly, with number, string etc, because it's already mixed types
 
-const circ = (diameter: number) =>{
-  return diameter * Math.PI 
+// objects
+let ninja = {
+  name:'mario',
+  belt: 'black',
+  age: 30
 }
-console.log(circ(7.5))
+ninja.age = 40 //ok
+ninja.name = 'ruy' //ok
+// ninja.age = '30' -> error: Type 'string' is not assignable to type 'number'.
+// ninja.skills = ['fighting,sneaking'] error-> Property 'skills' does not exist on type '{ name: string; belt: string; age: number; }'.
 
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  /* age:40 */ // error: Property 'age' is missing in type '{ name: string; belt: string; }' but required in type '{ name: string; belt: string; age: number; }'. if we remove one of the property, we have an error, because we declared it above, and if we want to change them, we need to write every property, no exception.
+  age: 40
+  //skills:[] -> error: Object literal may only specify known properties, and 'skills' does not exist in type '{ name: string; belt: string; age: number; }'. the same thing, we can't add unknown properties.
+}
