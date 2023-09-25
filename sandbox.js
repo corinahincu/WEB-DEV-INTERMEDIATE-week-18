bffcd41ed83ca04c19fe3fd6257356e62370fcd0
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-25 16:22:42 +0200

    Add files via upload

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,13 @@
 var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 30;
+var isBlackBelt = false;
+//character = 20 - we can change the value, see the next line, but not the type, if I try to change the type of character, I have an error, but if I change the value, it's working .
+character = 'mario'; // ok
+//age = 'yoshi' errror: Type 'string' is not assignable to type 'number'
+age = 40; // ok
+// isBlackBelt = 'yes' -> error : Type 'string' is not assignable to type 'boolean'
+isBlackBelt = true; // ok
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,17 @@
+let character = 'luigi'
+let age = 30
+let isBlackBelt = false 
 
-const character = 'luigi'
-console.log(character)
+//character = 20 - we can change the value, see the next line, but not the type, if I try to change the type of character, I have an error, but if I change the value, it's working .
+character = 'mario' // ok
+//age = 'yoshi' errror: Type 'string' is not assignable to type 'number'
+age = 40 // ok
 
-const inputs = document.querySelectorAll('input')
-console.log(inputs)
+// isBlackBelt = 'yes' -> error : Type 'string' is not assignable to type 'boolean'
+isBlackBelt = true // ok
 
-inputs.forEach(input => {
-  console.log(input) 
-})
+const circ = (diameter: number) =>{
+  return diameter * Math.PI 
+}
+console.log(circ(7.5))
+
