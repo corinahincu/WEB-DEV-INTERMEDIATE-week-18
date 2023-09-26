25cb8a06d06abea01d26b5d68ae58db1b810b320
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-27 00:36:59 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,27 +1,5 @@
-"use strict";
-// PUBLIC , PRIVATE, READONLY
-// default behaviour is that class invoice are public, anyone can change it.If we want to make it private, we can add private in front of it. for ex. private details. but in that case obviously we can't acces it outside of the class, i mean we can access it inside of the class, but outside no. But we can access it from format. 
-// readonly, means that we can read and access it but we can't change it
-class Invoice {
-    // 1 way:
-    /*   readonly client: string
-      private details: string
-      public amount: number */
-    /*   constructor(c:string,d:string,a:number){
-        this.client = c
-        this.details = d
-        this.amount = a
-      } */
-    // 2 way: 
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    } // from here
-}
+// MODULES
+import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the mario website', 300);
 let invoices = [];
