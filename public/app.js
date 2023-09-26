69058a9f6ed41a8ba24623087a4cf00608bde778
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-26 16:28:12 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,21 +1,25 @@
 "use strict";
-// THE DOM & TYPE CASTING
-// example 1 
-/* 1 way:
-const anchor = document.querySelector('a')
-  if(anchor){
-  console.log(anchor.href)
-}  */
-// 2 way:
-const anchor = document.querySelector('a');
-console.log(anchor.href);
-// if we a certain the anchor exists in our html file, we can add "!" at the end, and the errors disappears 
-// example 2:
-/* const form = document.querySelector('form')!
--> in that case is form element
- */
-const form = document.querySelector('.new-item-form'); // in that case  .new-item-form is a type element , so we add : as HTMLFormElement, to specify what kind of element. 
-// this example is useful because in the first case, we are not sure that it will retrieve the right form element, if we have for example more than one, so the second case, we are more specific.
+// CLASSES
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the mario website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+// we change properties in a class
+console.log(invoices);
+const form = document.querySelector('.new-item-form');
 /* console.log(form.children) */
 // inputs
 const type = document.querySelector('#type');
