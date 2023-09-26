76d87dc9ed610bd4a9a9ccd34732eec80adf72c5
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-26 14:44:36 +0200

    Add files via upload

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,30 +1,8 @@
 "use strict";
-// FRUNCTION BASICS
-/* let greet = () =>{
-  console.log('hello, world')
-}
-greet = 'hello'  */
-// Type 'string' is not assignable to type '() => void'
-// we can't assign writing the function in that way
-let greet;
-greet = () => {
-    console.log('hello,again');
+// TYPE ALIASES
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-// in that way it works
-const add = (a, b, c = 10) => {
-    console.log(a + b);
-    console.log(c);
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-add(5, 10, '20');
-// we can add c?: number|string -> without specifying the default parameter, but in that case in console.log we will see undefined.
-// if we add c: number|string = 10, then in this case we specify default parameter, and in console.log we'll see 10
-// but if we add add(5,10,'20') the third parameter, even string o number, doesn't matter, it overwrites , and in console.log we'll see 20 
-// optional parameters need to be added and the end
-// void -> a complete lack of any value ?
-const minus = (a, b) => {
-    return a + b;
-};
-/* const minus = (a:number, b:number):number => {
-  return a + b
-}  */ // this case :number at the end is optional, because typescript, does it automatically, but sometimes is a good practice to do it
-let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,32 +1,27 @@
-// FRUNCTION BASICS
-/* let greet = () =>{
-  console.log('hello, world')
-}
-greet = 'hello'  */
-// Type 'string' is not assignable to type '() => void'
-// we can't assign writing the function in that way
+// TYPE ALIASES
 
-let greet: Function
-greet = () =>{
-  console.log('hello,again')
+/* const logDetails = (uid: string | number, item: string) =>{
+  console.log(`${item} has a uid of ${uid}`)
 }
-// in that way it works
 
-const add = (a:number,b:number, c: number|string = 10):void =>{
-  console.log(a + b)
-  console.log(c)
+const greet = (user: {name: string, uid:string | number}) => {
+  console.log(`${user.name} says hello`)
 }
-add(5,10,'20')
-// we can add c?: number|string -> without specifying the default parameter, but in that case in console.log we will see undefined.
-// if we add c: number|string = 10, then in this case we specify default parameter, and in console.log we'll see 10
-// but if we add add(5,10,'20') the third parameter, even string o number, doesn't matter, it overwrites , and in console.log we'll see 20 
-// optional parameters need to be added and the end
-// void -> a complete lack of any value ?
 
-const minus = (a:number, b:number) => {
-  return a + b
-} 
-/* const minus = (a:number, b:number):number => {
-  return a + b
-}  */ // this case :number at the end is optional, because typescript, does it automatically, but sometimes is a good practice to do it
-let result = minus(10,7)
+const greetAgain = (user: {name: string, uid:string | number}) => {
+  console.log(`${user.name} says hello`)
+} */
+// in that case we write the same code over and over again, to go around that we can make our own type aliases, to define them first, and use them as many times we want in different functions :
+//  |
+//  v
+
+type StringOrNum = string | number
+type objWithName = { name: string, uid: StringOrNum}
+
+const logDetails = (uid: StringOrNum, item: string) =>{
+  console.log(`${item} has a uid of ${uid}`)
+}
+
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello`)
+}
