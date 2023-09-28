b205f1c11f513f339b4970e751ce2c2417995e30
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-28 17:15:17 +0200

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const amount = document.querySelector('#amount') as HTMLInputElement
 const ul = document.querySelector('ul')!
 const list = new listTemplate(ul)
 
-form.addEventListener('submit', (e: Event)=>{
+/* form.addEventListener('submit', (e: Event)=>{
   e.preventDefault()
   
   let doc: hasFormatter
@@ -28,66 +28,35 @@ form.addEventListener('submit', (e: Event)=>{
     doc = new Payment(toFrom.value,details.value,amount.valueAsNumber)
   }
   list.render(doc,type.value,'end')
+}) */
+
+form.addEventListener('submit', (e: Event)=>{
+  e.preventDefault()
+  // using tuples
+  let values: [string,string,number]
+  values = [toFrom.value,details.value,amount.valueAsNumber]
+
+  let doc: hasFormatter
+  if(type.value === 'invoice'){
+    doc = new Invoice(...values)
+  } else {
+    doc = new Payment(toFrom.value,details.value,amount.valueAsNumber)
+  }
+  list.render(doc,type.value,'end')
 })
+// tuples
+// are little bit like arrays, we also can define them in square brackets, but there is one major difference, the types of data in each position in a tuple is fixed once it's been initialized
 
+let arr = ['ruy', 25, true]
+arr[0] = false 
+arr[1] = 'yoshi'
+arr = [30,false, 'yoshi']
 
-// GENERICS 
-// allows us to create reusable blocks of code which can be used with different types
+let tup: [string, number, boolean] = ['ruy', 25, true]
+// once each position has been defined , as a type like this, you can't change them around,it can't be anything else, or you'll have an error
+tup[0] = 'ken'
+/* tup[0] = false  */// error: Type 'boolean' is not assignable to type 'string'.
+tup[1] = 30
 
-/* const addUID = <T extends object>(obj: T) => {
-  let uid = Math.floor(Math.random()* 100)
-  return {...obj,uid} // returnes new obj
-} */
-
-/* let docOne = addUID({name:'yoshi', age:40}) */
-/* console.log(docOne) */ // result: random UID
-/* let docTwo = addUID('hello') */
-/* console.log(docOne.name) */ // in that case we have an error, because when we pass in an obj into the function we're not specifying exactly what this obj should be right and it doesn't know when it returns this new obj and what properties were on the obj that we passed in,in other words in doesn't know if a name or age exists on the objects , because we've not said that anywhere, and it's not captured those inside the functions. So we can combat this by using the GENERIC <T>. in that case it works, but we are not specifying that the type should be an object, so in that case we can add also a string, but in case that we don't want that, like in our example, we want to specify that we can add just objects, we can add like this: <T extends object>(obj: T), we can be also more specific, we can specify which type of object it's allowed to be add, for example <T extends {name: string}>(obj: T)
-
-// GENERICS WITH INTERFACES
-
-/* interface Resource<T> {
-  uid: number
-  resourceName: string
-  data: T // we want to make it generic and fle
-} */
-
-/* const docThree: Resource<string> = {
-  uid: 1,
-  resourceName: 'person',
-  data: 'shaun'
-}  */
-
-/* const docFour: Resource<object> = {
-  uid: 1,
-  resourceName: 'person',
-  data: {name:'shaun'}
-} 
- */
-/* const docFive: Resource<string[]> = {
-  uid:2,
-  resourceName:'shoppingList',
-  data:['bread','milk','toilet rolls']
-} */
-
-// ENUMS 
-// are special type  in type script, wich allows to store a set of constants or keyword, and associate them with a numeric value
-enum resourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
-interface Resource<T> {
-  uid: number
-  resourceType: number
-  data: T 
-} 
-
-const docOne: Resource<object> = {
-  uid:1,
-  resourceType:resourceType.BOOK,
-  data: {title:'name of the wind'}
-}
-
-const docTwo: Resource<object> = {
-  uid:10,
-  resourceType:resourceType.PERSON,
-  data: {name:'yoshi'}
-}
-console.log(docOne,docTwo)
+let student: [ string, number]
+student = ['chun', 234]
