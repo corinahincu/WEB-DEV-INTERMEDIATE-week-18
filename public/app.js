94f517096efbcaa3f729b2122e5bbbf9f639d903
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-28 12:44:30 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,36 +1,17 @@
-// INTERFACES with classes
+// RENDERING AN HTML TEMPLATE
 // MODULES
 import { Invoice } from './classes/invoice.js';
+import { listTemplate } from './classes/listTemplates.js';
 import { Payment } from './classes/payment.js';
-let docOne;
-let docTwo;
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payment('mario', 'plumbing work', 200);
-// if we want to create an array that only holds object which implements hasFormatter interface, we can do it like this:
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-console.log(docs);
-// in that case, we just restricting what it can go in docs
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the mario website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-/* invOne.client = 'yoshi'
-invTwo.amount = 400 */
-// we change properties in a class
-/* console.log(invoices) */
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format()); // and here
-});
 const form = document.querySelector('.new-item-form');
-/* console.log(form.children) */
 // inputs
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#toFrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new listTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -40,5 +21,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
---
+++ public/classes/listTemplates.js
@@ -0,0 +1,28 @@
+export class listTemplate {
+    constructor(container) {
+        this.container = container;
+        // we don't have to do anything inside here, because when we have private in front of the property it automatically assign whatever value would pass in as the argument to a property on that instance
+    }
+    render(item, heading, pos) {
+        const li = document.createElement('li');
+        const h4 = document.createElement('h4');
+        h4.innerText = heading;
+        li.append(h4);
+        const p = document.createElement('p');
+        p.innerText = item.format(); // returnes formatting string
+        li.append(p);
+        if (pos === 'start') {
+            this.container.prepend(li);
+        }
+        else {
+            this.container.append(li);
+        }
+    }
+}
+/*
+1. register a list container (ul) in the constructor
+2. create a render method to render a new 'li' to the container
+    --- accepts arguments: invoice or payment, a heading, a position
+    --- create the html template(li,h4,p)
+    --- add the 'li' template to the start/end of the list
+*/ 
