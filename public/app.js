6f21a24c2efa96ddabb8b450d43a33bfae71878b
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-09-28 14:30:10 +0200

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -25,26 +25,54 @@ form.addEventListener('submit', (e) => {
 });
 // GENERICS 
 // allows us to create reusable blocks of code which can be used with different types
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid }); // returnes new obj
-};
-let docOne = addUID({ name: 'yoshi', age: 40 });
+/* const addUID = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random()* 100)
+  return {...obj,uid} // returnes new obj
+} */
+/* let docOne = addUID({name:'yoshi', age:40}) */
 /* console.log(docOne) */ // result: random UID
 /* let docTwo = addUID('hello') */
-console.log(docOne.name); // in that case we have an error, because when we pass in an obj into the function we're not specifying exactly what this obj should be right and it doesn't know when it returns this new obj and what properties were on the obj that we passed in,in other words in doesn't know if a name or age exists on the objects , because we've not said that anywhere, and it's not captured those inside the functions. So we can combat this by using the GENERIC <T>. in that case it works, but we are not specifying that the type should be an object, so in that case we can add also a string, but in case that we don't want that, like in our example, we want to specify that we can add just objects, we can add like this: <T extends object>(obj: T), we can be also more specific, we can specify which type of object it's allowed to be add, for example <T extends {name: string}>(obj: T)
-const docThree = {
+/* console.log(docOne.name) */ // in that case we have an error, because when we pass in an obj into the function we're not specifying exactly what this obj should be right and it doesn't know when it returns this new obj and what properties were on the obj that we passed in,in other words in doesn't know if a name or age exists on the objects , because we've not said that anywhere, and it's not captured those inside the functions. So we can combat this by using the GENERIC <T>. in that case it works, but we are not specifying that the type should be an object, so in that case we can add also a string, but in case that we don't want that, like in our example, we want to specify that we can add just objects, we can add like this: <T extends object>(obj: T), we can be also more specific, we can specify which type of object it's allowed to be add, for example <T extends {name: string}>(obj: T)
+// GENERICS WITH INTERFACES
+/* interface Resource<T> {
+  uid: number
+  resourceName: string
+  data: T // we want to make it generic and fle
+} */
+/* const docThree: Resource<string> = {
+  uid: 1,
+  resourceName: 'person',
+  data: 'shaun'
+}  */
+/* const docFour: Resource<object> = {
+  uid: 1,
+  resourceName: 'person',
+  data: {name:'shaun'}
+}
+ */
+/* const docFive: Resource<string[]> = {
+  uid:2,
+  resourceName:'shoppingList',
+  data:['bread','milk','toilet rolls']
+} */
+// ENUMS 
+// are special type  in type script, wich allows to store a set of constants or keyword, and associate them with a numeric value
+var resourceType;
+(function (resourceType) {
+    resourceType[resourceType["BOOK"] = 0] = "BOOK";
+    resourceType[resourceType["AUTHOR"] = 1] = "AUTHOR";
+    resourceType[resourceType["FILM"] = 2] = "FILM";
+    resourceType[resourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    resourceType[resourceType["PERSON"] = 4] = "PERSON";
+})(resourceType || (resourceType = {}));
+const docOne = {
     uid: 1,
-    resourceName: 'person',
-    data: 'shaun'
+    resourceType: resourceType.BOOK,
+    data: { title: 'name of the wind' }
 };
-const docFour = {
-    uid: 1,
-    resourceName: 'person',
-    data: { name: 'shaun' }
+const docTwo = {
+    uid: 10,
+    resourceType: resourceType.PERSON,
+    data: { name: 'yoshi' }
 };
-const docFive = {
-    uid: 2,
-    resourceName: 'shoppingList',
-    data: ['bread', 'milk', 'toilet rolls']
-};
+console.log(docOne, docTwo);
